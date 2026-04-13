ac277528ce887f82a6648224e022fd9e028325c1
Aparna Mounika Anupoju <manupoju@msa.miraclesoft.com>
2026-04-13 15:39:53 +0530

    first commit

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+VERSION = os.getenv("VERSION", "v1")
+
 @app.route("/")
 def home():
-    return "Hello, Working on a task related to ArgoCD to automate application deployments by integrating it into the CI/CD pipeline."
+    return f"Hello, Working on a task related to ArgoCD to automate application deployments by integrating it into the CI/CD pipeline | Version: {VERSION}"
 
 @app.route("/health")
 def health():
-    return {"status": "healthy"}
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
